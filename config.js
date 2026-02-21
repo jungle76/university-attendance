8c1c9c3e373b60cb657b8af8c7d09cd5f809b053
Ginola <jungle1976@gmail.com>
2026-02-21 21:06:42 +0200

    Lista studenti finalizata si configurare cursuri

--- config.js
+++ config.js
@@ -10,32 +10,32 @@ const COURSES = {
     id: 2,
     name: "Probabilitați și statistică",
     shortName: "ACRONYM2",
-    scriptUrl: "https://script.google.com/macros/s/AKfycbxKsKI9pRe5dlAxNl92taAIzqVRYmHhaMh251HfV2Oik-mT_c3TAcbyvaiY20RS2r9Fgw/exec",
+    scriptUrl: "https://script.google.com/macros/s/AKfycbxm4aKaYw6N_dh7K-ZaPvMQIKEflsAlcQhghwSsaRN7Jn4GPt4-gVz-SWzFN95nmsCv/exec",
   },
  
   3: {
     id: 3,
     name: "Geometrie computațională",
     shortName: "ACRONYM2",
-    scriptUrl: "https://script.google.com/macros/s/AKfycbxKsKI9pRe5dlAxNl92taAIzqVRYmHhaMh251HfV2Oik-mT_c3TAcbyvaiY20RS2r9Fgw/exec",
+    scriptUrl: "https://script.google.com/macros/s/AKfycbxgjhbCH9h9Fe7G0liqBFIWyM_hV6TgUC5XbYVTcgHwSZEck8s4MpCZ6SqsqWDRt6U/exec",
   },
-
-    4: {
+  4: {
     id: 4,
     name: "Automate, calculabilitate si complexitate",
     shortName: "ACRONYM2",
-    scriptUrl: "https://script.google.com/macros/s/AKfycbxKsKI9pRe5dlAxNl92taAIzqVRYmHhaMh251HfV2Oik-mT_c3TAcbyvaiY20RS2r9Fgw/exec",
+    scriptUrl: "https://script.google.com/macros/s/AKfycbzBKH1TOQglJk2GwLWrJqyc-kw8wZYdTfHIBe2v-hpCzGJcgklkqvTPLUaVxzgkra7FLQ/exec",
   },
    5: {
     id: 5,
     name: "Sisteme de gestiune a bazelor de date",
     shortName: "ACRONYM2",
-    scriptUrl: "https://script.google.com/macros/s/AKfycbxKsKI9pRe5dlAxNl92taAIzqVRYmHhaMh251HfV2Oik-mT_c3TAcbyvaiY20RS2r9Fgw/exec",
+    scriptUrl: "https://script.google.com/macros/s/AKfycbyZ8S1pqrQ81k5Usf2JhEASzK0qCJPwF3j4IWXgugJ5ijDHLnuXwGs4hJ64IwsGl4o/exec",
   },
-}; 
+};
+
 // Campus location for geolocation check
 const CAMPUS = {
-  lat: 38.878954558014385,
+  lat: 46.198582099764764,
   lon: -84.69438504752182,
   radiusMeters: 100,
 };
@@ -44,8 +44,24 @@ const CAMPUS = {
 const ENABLE_AUTOCOMPLETE = true; // Set to false to disable name autocomplete
 
 const STUDENTS = [
-  "STUDENT NAME 1",
-  "STUDENT NAME 2",
-  "STUDENT NAME 3",
+"AILUȚOAEI ANDREI-ADELIN", "ANGHEL DAVID-MIHAI", "ARDELEAN DANIEL-DEMETRIUS",
+"BANCI ALIN-DAVID", "BIRIȘ DENIS-FLORIN", "BONCACIU DANIELA-MIHAELA",
+"BORTEȘ AMELIA-NARCISA", "BRADIN ALEXANDRA-VALENTINA", "BRANC NATALIA-ALEXANDRA",
+"BREHA CRINA-LARISA", "BURUIANA MARIUS-FLORENTIN", "CHERESI-DOLGA NICOLA-CRISTINA",
+"CIOTOROSCHI FLAVIUS-FLORIN", "CLOP PAUL-EDUARD", "COVACI DENIS-EMANUEL",
+"CRĂCIUN DENIS-GABRIEL", "CRISTEA ANDRE", "CRISTEA ROBERT-SAMUEL",
+"DIMA DENIS-NICOLAE-IOAN", "FELEA DORIN-ANDREI", "FIȚ RAFAEL",
+"GAI MARIUS-CRISTIAN", "GHERDAN ALIN", "ILEA LETIȚIA", "ISHIMWE AIME BRUCE",
+"KIRCH ROLAND-NORBERT", "KÖTELES PATRICK", "LĂPUGEAN SAMUEL",
+"LEORINȚ ANDREEA-MARIA", "LILE ANDREEA-AMALIA", "MARIAȘIU TEODORA-ANDREEA",
+"MATEI BOGDAN-OVIDIU", "METOUGUE GAETAN CHRISTIAN", "MOR-BARTL ERIK",
+"MOR-BARTL ROBERT", "MORAR ANDREI-GEORGE", "NĂDĂBAN RAUL-DORIN",
+"NEDELCU ȘTEFAN-EUGEN", "OANCEA DANIEL", "PELE ANDREEA", "PETRUȚA BIANCA-MARIA",
+"POLEAC ALEX-FLORIN", "RAȚ ADRIAN-IOAN", "SANTA FLAVIUS-FRANCISC",
+"SENTIVEANU MARIUS-CLAUDIU", "SIDOR OANA-BOGDANA", "SOLONCĂ ANA-MARIA",
+"SPĂTARU ANDI-FLORIAN", "TOMESCU ROBERT-GABRIEL", "TOTH EVELINE-DENISA",
+"TUDOR ESTERA", "TUDOR RAHELA", "ȚUCUDEAN FABIAN", "URSACHE CLAUDIA-MINA",
+"VARGA NORBERT-FLAVIUS"
+
   // Add more student names here
 ].sort();

e5c7340a7b71e027420ab8bd6fe90c074b96818a
Ginola <jungle1976@gmail.com>
2026-02-21 12:56:45 +0200

    Am adăugat cursuri noi în config.js

--- config.js
+++ config.js
@@ -2,18 +2,37 @@
 const COURSES = {
   1: {
     id: 1,
-    name: "COURSE NAME 1",
+    name: "Dezvoltarea aplicațiilor mobile",
     shortName: "ACRONYM1",
-    scriptUrl: "https://script.google.com/macros/s/YOUR_SCRIPT_URL_1/exec",
+    scriptUrl: "https://script.google.com/macros/s/AKfycbxKsKI9pRe5dlAxNl92taAIzqVRYmHhaMh251HfV2Oik-mT_c3TAcbyvaiY20RS2r9Fgw/exec",
   },
   2: {
     id: 2,
-    name: "COURSE NAME 2",
+    name: "Probabilitați și statistică",
     shortName: "ACRONYM2",
-    scriptUrl: "https://script.google.com/macros/s/YOUR_SCRIPT_URL_2/exec",
+    scriptUrl: "https://script.google.com/macros/s/AKfycbxKsKI9pRe5dlAxNl92taAIzqVRYmHhaMh251HfV2Oik-mT_c3TAcbyvaiY20RS2r9Fgw/exec",
   },
-};
+ 
+  3: {
+    id: 3,
+    name: "Geometrie computațională",
+    shortName: "ACRONYM2",
+    scriptUrl: "https://script.google.com/macros/s/AKfycbxKsKI9pRe5dlAxNl92taAIzqVRYmHhaMh251HfV2Oik-mT_c3TAcbyvaiY20RS2r9Fgw/exec",
+  },
 
+    4: {
+    id: 4,
+    name: "Automate, calculabilitate si complexitate",
+    shortName: "ACRONYM2",
+    scriptUrl: "https://script.google.com/macros/s/AKfycbxKsKI9pRe5dlAxNl92taAIzqVRYmHhaMh251HfV2Oik-mT_c3TAcbyvaiY20RS2r9Fgw/exec",
+  },
+   5: {
+    id: 5,
+    name: "Sisteme de gestiune a bazelor de date",
+    shortName: "ACRONYM2",
+    scriptUrl: "https://script.google.com/macros/s/AKfycbxKsKI9pRe5dlAxNl92taAIzqVRYmHhaMh251HfV2Oik-mT_c3TAcbyvaiY20RS2r9Fgw/exec",
+  },
+}; 
 // Campus location for geolocation check
 const CAMPUS = {
   lat: 38.878954558014385,
